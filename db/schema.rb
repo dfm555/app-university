208aca4b42bc82a90ced8008a94538656c75f7aa
dfm555 <dfm556@gmail.com>
2016-08-05 01:01:08 -0500

    fixed issue heroku db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805014038) do
+ActiveRecord::Schema.define(version: 20160805014039) do
 
   create_table "rols", force: :cascade do |t|
     t.string   "name"
